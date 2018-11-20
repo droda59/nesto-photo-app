285caf467c91d49d4d46fdeca947191e89fa8893
Maxime Gaudreault Proulx <maxime.gaudreault@ssense.com>
2018-11-20 13:24:22 -0500

    Fixed the route handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { Router, browserHistory } from 'react-router';
-import Routes from './Routes';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router';
+import Home from './login/LoginPage';
+import AlbumPage from './albums/AlbumPage';
+import PostPage from './posts/PostPage';
+import NotFound from './shared/NotFound';
+import { ROUTE_HOME } from './constants/routes';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <Router routes={Routes} history={browserHistory} />
-    );
-  }
+export default class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/albums' component={AlbumPage} />
+                    <Route path='/posts' component={PostPage} />
+                    <Route component={NotFound} />
+                </Switch>
+            </BrowserRouter>
+        );
+    }
 }
-
-export default App;
